488a61214b90e3688e7c39aa0db889a5096655df
Matic Kozel <matic.kozel@lancom.si>
2023-12-27 13:51:30 +0100

    Added Geoapify token

--- core/src/com/mygdx/game/utils/Keys.java
+++ core/src/com/mygdx/game/utils/Keys.java
@@ -2,5 +2,5 @@ package com.mygdx.game.utils;
 
 public class Keys {
     public static String MAPBOX = "";
-    public static String GEOAPIFY = "";
+    public static String GEOAPIFY = "8767f3df37c347ad93f182d5a528f071";
 }
